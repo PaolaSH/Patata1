c5ef55a66984b85370a777300b2fee5148f666fe
PaolaSH <klararoma3@gmail.com>
2022-07-16 12:08:16 +0200

    Prospsusestate

--- src/componentes/Hola.js
+++ src/componentes/Hola.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { useState } from "react";
 import Contador from "./Ccontador";
+import Props1 from "./Props";
 
 
 
 const Hola = () => {
 const [valor, update]= useState (22)
 
-const tocar = () => {update(valor + 2)}
+const tocar = () => {update(valor + 1)}
 /*
 const contador = useState(0)
  const valor = contador [0]
  const update = contador [1]  */
+const reset = () => { update (0)}
 
-
+const isEven = valor % 2 === 0
+const op2 = isEven ? "es par" : "no es par" 
 
 return (
     <div>
+
         <h1>{valor}</h1>
+
         <button onClick={tocar}>
             
             mas2
         </button>
+        <p>{op2}</p>
+        <button onClick={ reset }>
+            reset
+        </button>
+        <Props1 number = {valor} />
     </div>
 )
 }
---
+++ src/componentes/Props.js
@@ -0,0 +1,11 @@
+
+
+
+
+const Props1 = ({number}) => {
+    console.log("se rederiso")
+    return ( <h2>{number}</h2> );
+}
+ 
+export default Props1
+;
